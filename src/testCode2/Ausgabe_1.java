0b8f4939439b34ad30302437b4e6f293bd28af7c
Matthaeus-Erd <Matthaeus.erd@hak-reutte.ac.at>
2024-01-15 10:31:02 +0100

    Ausgabe 1/2

---
+++ src/testCode2/Ausgabe_1.java
@@ -0,0 +1,12 @@
+package testCode2;
+
+public class Ausgabe_1 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Test1");
+		System.out.println("Test2");
+
+	}
+
+}
---
+++ src/testCode2/Ausgabe_2.java
@@ -0,0 +1,12 @@
+package testCode2;
+
+public class Ausgabe_2 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Test3");
+		System.out.println("Test4");
+
+	}
+
+}
