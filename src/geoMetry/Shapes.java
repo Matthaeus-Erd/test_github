918262a735ca043754a51f551cb8facf4b04b382
Matthaeus-Erd <Matthaeus.erd@hak-reutte.ac.at>
2024-01-22 10:13:21 +0100

    Ausimplementierung der Klassen Circle, Coordinate und Shapes

--- src/geoMetry/Shapes.java
+++ src/geoMetry/Shapes.java
@@ -1,7 +1,34 @@
 package geoMetry;
 
+import java.util.ArrayList;
+
 public abstract class Shapes {
 
+	private ArrayList<Shapes> shapes;
+	double circumference;
+	double area;
+	
+	public abstract double calculateCircumferenece();
+	public abstract double calculateArea();
+	
+	public ArrayList<Shapes> getShapes() {
+		return shapes;
+	}
+	public void setShapes(ArrayList<Shapes> shapes) {
+		this.shapes = shapes;
+	}
+	public double getCircumference() {
+		return circumference;
+	}
+	public void setCircumference(double circumference) {
+		this.circumference = circumference;
+	}
+	public double getArea() {
+		return area;
+	}
+	public void setArea(double area) {
+		this.area = area;
+	}
 	
 	
 }
