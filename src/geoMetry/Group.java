002a480efee5416aafb00f3588ec73a7789311ed
Matthaeus-Erd <Matthaeus.erd@hak-reutte.ac.at>
2024-01-29 11:45:04 +0100

    Beginn anlegung arraylist Shapes

--- src/geoMetry/Group.java
+++ src/geoMetry/Group.java
@@ -1,5 +1,22 @@
 package geoMetry;
 
-public class Group {
+import java.util.ArrayList;
 
+public class Group {
+	
+	public ArrayList<Shapes> shapes;
+	
+	public Group() {
+		this.shapes = new ArrayList<Shapes>();
+	}
+
+	public ArrayList<Shapes> getShapes() {
+		return shapes;
+	}
+
+	public void setShapes(ArrayList<Shapes> shapes) {
+		this.shapes = shapes;
+	}
+	
+	
 }
