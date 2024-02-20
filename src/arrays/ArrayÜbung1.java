c49d2fb6aa0eca4155dd09d2da64e8e8fde8c1a5
Matthaeus-Erd <Matthaeus.erd@hak-reutte.ac.at>
2024-02-20 09:14:01 +0100

    Erstellung der Arrayübungs Klassen

---
+++ src/arrays/ArrayÜbung1.java
@@ -0,0 +1,12 @@
+package arrays;
+
+public class ArrayÜbung1 {
+
+	int[] dailyGains = { 1000, 2000, 500, 9000, 9010 };
+	
+	int count5PercentJumps(int[]gains) {
+		int Umsatzsprünge = 0;
+		if()
+	}
+	
+}
