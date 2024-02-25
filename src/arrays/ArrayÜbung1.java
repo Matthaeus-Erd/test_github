de023013273a7fe7ac2dc6944f488ffd4a1a3efd
Matthaeus-Erd <Matthaeus.erd@hak-reutte.ac.at>
2024-02-25 22:17:21 +0100

    Ausprogrammierung der Aufgabe 1

--- src/arrays/ArrayÜbung1.java
+++ src/arrays/ArrayÜbung1.java
@@ -2,18 +2,37 @@ package arrays;
 
 public class ArrayÜbung1 {
 
-	int day;
-	int nextday;
-	int percentjump;
-	int[]dailyGains = new int[31];
+	private static int day;
+	private static int nextDay;
+	private static double percentJump;
+	private static int percentJumpCount;
+	private static int[]dailyGains = new int[31];
 	
-	
-	int count5PercentJumps(int[]gains) {
+	public static int count5PercentJumps(int[]gains) {
 		for(int i = 0; i < dailyGains.length; i++) {
 			day = dailyGains[i];
-			nextday = dailyGains[i+1];
-			
+			if(i >= dailyGains.length) {
+				System.err.println("Letzter Tag");
+				nextDay = dailyGains[0];
+			}else {
+				nextDay = dailyGains[i +1];
+			}
+			percentJump = nextDay * 0.05;
+			if((nextDay - day) >= percentJump) {
+				percentJumpCount ++;
+			}
 		}
+		return percentJumpCount;
 	}
 	
+	public static void main (String[] args) {
+		dailyGains[0] = 1000;
+		dailyGains[1] = 2000;
+		dailyGains[2] = 500;
+		dailyGains[3] = 9000;
+		dailyGains[4] = 9010;
+		
+		int count = count5PercentJumps(dailyGains);
+		System.out.println(count);
+	}
 }
