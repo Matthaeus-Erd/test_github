db79635ffbadc1884ad710451bb22c330c2a9148
Matthaeus-Erd <Matthaeus.erd@hak-reutte.ac.at>
2024-02-26 11:45:22 +0100

    Erstellung und Bearbeitung der Klasse für die Aufgabe

---
+++ src/arrays/AufgabeBildVergrößern.java
@@ -0,0 +1,23 @@
+package arrays;
+
+public class AufgabeBildVergrößern {
+	
+	static int[][]matrix = new int[2][3];
+	static int[][]matrix2 = new int[4][6];
+	
+	public static int[][]magnify(int[][]array, int factor){
+		for(int i = 0; i < matrix.length; i++) {
+			for(int j = 0; j < matrix[0].length; j++) {
+				
+			}
+		}
+		return array;
+	}
+	
+	public static void main(String[] args) {
+
+		
+		
+	}
+
+}
