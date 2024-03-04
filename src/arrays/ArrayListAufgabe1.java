5f918efca13599b87843800dae76eafcaf5457ce
Matthaeus-Erd <Matthaeus.erd@hak-reutte.ac.at>
2024-03-04 11:44:20 +0100

    Erweiterung der Klasse ArrayListAufgabe1

--- src/arrays/ArrayListAufgabe1.java
+++ src/arrays/ArrayListAufgabe1.java
@@ -1,8 +1,12 @@
 package arrays;
 
+import java.lang.reflect.Array;
+import java.util.List;
+
 public class ArrayListAufgabe1 {
 
 	public static void reduceToComments(List<String>lines) {
+		
 		
 	}
 	
