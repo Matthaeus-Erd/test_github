ccdd5fcb5d53cb7e6f2eec263c0a7b7ccb5700f6
Matthaeus-Erd <Matthaeus.erd@hak-reutte.ac.at>
2024-04-07 23:29:43 +0200

    Homework

--- src/queue/ArrayQueue.java
+++ src/queue/ArrayQueue.java
@@ -14,6 +14,9 @@ public class ArrayQueue {
 		queue = new int[size];
 	}
 	
+	/*
+	 * Adds and item into the queue
+	 */
 	public static void queueEnqueue(int item) {
 		if(rear == capacity) {
 			System.out.println("Queue ist voll.");
@@ -23,13 +26,51 @@ public class ArrayQueue {
 		}
 	}
 	
+	/*
+	 * Removes an item with an ID form the queue
+	 */
 	public static int queueDequeue() {
+		int firstElement = 0;
 		if(rear == front) {
 			System.out.println("Queue ist leer");
 		}else {
-			int firstElement = queue[front];
+			firstElement = queue[front];
+			for(int i = 0; i < (rear-1); i++) {
+				queue[i] = queue[i+1];
+			}
+			if(rear < capacity) {
+				queue[rear] = 0;
+			}
+			rear--;
 			
 		}
+		return firstElement;
+	}
+	
+	/*
+	 * Prints all the items in the queue
+	 */
+	public static void queueDisplay() {
+		if(front == rear) {
+			System.out.println("Queue is empty!");
+		}else {
+			for(int i = front; i < rear; i++) {
+				System.out.println(i);
+			}
+		}
+	}
+	
+	/*
+	 * Returns the first element in the queue
+	 */
+	public static int queueFront() {
+		int firstElement = 0;
+		if(rear == front) {
+			System.out.println("Queue is empty!");
+		}else {
+			firstElement = queue[front];
+		}
+		return firstElement;
 	}
 	
 }
---
+++ src/queue/ArrayQueueTest.java
@@ -0,0 +1,16 @@
+package queue;
+
+public class ArrayQueueTest {
+
+	public static void main(String[] args) {
+		
+		ArrayQueue queue = new ArrayQueue(10);
+		
+		for(int i = 0; i < 10; i++) {
+			queue.queueEnqueue(i);
+			queue.queueDisplay();
+		}
+		queue.queueEnqueue(1);
+	}
+
+}
