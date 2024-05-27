a641102f51c4610aed246784d2fed3805533a138
Matthaeus-Erd <Matthaeus.erd@hak-reutte.ac.at>
2024-05-27 10:09:19 +0200

    Hausübung 27.05.2024

---
+++ src/exceptions/MusikNotenLeser.java
@@ -0,0 +1,25 @@
+package exceptions;
+	 
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
+	 
+	public class MusikNotenLeser {
+	 
+		public static void main(String[] args) {
+			Path filePath = Paths.get("file.txt");
+	 
+	        try (Scanner scanner = new Scanner(filePath)) {
+	            while (scanner.hasNextLine()) {
+	                String line = scanner.nextLine();
+	                System.out.println(line);
+	            }
+	        } catch (IOException e) {
+	            e.printStackTrace();
+	        }
+	 
+		}
+	 
+	}
+	
